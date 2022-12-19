2df28229552185a6c58328883d23a007d237fa36
Alexander Gorlenko <goral92@inbox.ru>
2022-12-19 17:06:43 +0300

    feat: добавит в проект eslint и конфиг RTI-2127

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:vue/vue3-essential',
+    'airbnb-base',
+    'plugin:nuxt/recommended',
+  ],
+  parser: 'vue-eslint-parser',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+  },
+  settings: {
+    'import/resolver': {
+      nuxt: {
+        extensions: ['.js', '.vue'],
+      },
+    },
+  },
+};
