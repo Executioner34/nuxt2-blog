ca348caa1a4e82e23a3b5b47af725220a2b31049
Alexander Gorlenko <goral92@inbox.ru>
2022-12-20 18:00:37 +0300

    feat: создает компонент Post для главной страницы RTI-2127

---
+++ components/views/index/Post.vue
@@ -0,0 +1,63 @@
+<template>
+  <el-card
+    shadow="hover"
+    :body-style="{ padding: 0}"
+    class="post"
+  >
+    <header slot="header" class="post-header">
+        <h3>Post title</h3>
+        <small>
+          <i class="el-icon-time"></i> {{ new Date().toLocaleString() }}
+        </small>
+    </header>
+    <div class="post-body">
+      <img src="https://traveller-eu.ru/sites/default/files/inline-images/2017-09-22%2009.50.03%202-800x533.jpg" alt="post image" class="post-img">
+    </div>
+    <footer class="post-footer">
+      <el-button round @click="openPost">Открыть</el-button>
+      <span><i class="el-icon-message"></i> 12</span>
+    </footer>
+  </el-card>
+</template>
+
+<script>
+/**
+ * @module components/view/index/Post.vue
+ * @desc статья для страницы home
+ * @vue-prop {type} name - desc
+ * @vue-data {type} name - desc
+ * @vue-computed {type} name - desc
+ */
+export default {
+  name: 'VPost',
+  methods: {
+    openPost() {
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.post {
+  margin-bottom: 1.5rem;
+
+  &-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  &-img {
+    width: 100%;
+    height: auto;
+  }
+
+  &-footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+  }
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,16 @@
 <template>
- <div></div>
+  <el-row type="flex" justify="center">
+    <el-col :xs="24" :sm="18" :md="12" :lg="10">
+      <index-post v-for="post in 3" :key="post"/>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  head: {
+    title: 'Главная',
+  },
 };
 </script>
