3153e1084a0ca3aec5ff5c154886522912b5ba2a
Alexander Gorlenko <goral92@inbox.ru>
2022-12-20 13:39:38 +0300

    feat: добавит в проект element-ui RTI-2127

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,11 +21,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-
+    'element-ui/lib/theme-chalk/index.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '@/plugins/globals',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -42,5 +43,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: [/^element-ui/],
   },
 };
