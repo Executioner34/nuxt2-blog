d823cfeed5b0438ddf409ff8994b8435de8dd21f
Alexander Gorlenko <goral92@inbox.ru>
2022-12-21 17:46:45 +0300

    feat: добавляет страницу 404 ошибки RTI-2133

---
+++ layouts/error.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="error-layout">
+    <div class="error-content">
+      <h1 class="error-title">Ошибка 404</h1>
+      <nuxt-link to="/">Вернуться на главную</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+/**
+ * @module layouts/error.vue
+ * @desc страница ошибки 404
+ */
+export default {
+  name: 'ErrorLayout',
+};
+</script>
+
+<style lang="scss" scoped>
+.error-layout {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.error-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 64px;
+}
+
+.error-title {
+  color: rgb(145, 6, 6);
+  margin-bottom: 16px;
+}
+</style>
