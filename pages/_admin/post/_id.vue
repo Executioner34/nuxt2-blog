4094a50afc549a1815a8381cfb9062ebf720c45b
Alexander Gorlenko <goral92@inbox.ru>
2022-12-27 15:08:51 +0300

    feat: добавит функционал редактирования поста RTI-2134

--- pages/_admin/post/_id.vue
+++ pages/_admin/post/_id.vue
@@ -1,5 +1,5 @@
 <template>
-  <PostPage/>
+  <PostPage :data="post"/>
 </template>
 
 <script>
@@ -7,13 +7,26 @@ import PostPage from '@/components/views/_admin/post/index.vue';
 /**
  * @module pages/_admin/post/_id.vue
  * @desc страница по роуту /admin/:id
+ * @vue-data {Object} post - объект редактируемого поста
  */
 export default {
   name: 'AdminPostPage',
   layout: 'admin',
   middleware: ['admin-auth'],
+  head() {
+    return {
+      title: `Пост | ${this.post.title}`,
+    };
+  },
+  validate({ params }) {
+    return Boolean(params.id);
+  },
   components: {
     PostPage,
+  },
+  async asyncData({ store, params }) {
+    const post = await store.dispatch('post/fetchAdminById', params.id);
+    return { post };
   },
 };
 </script>
--- store/post.js
+++ store/post.js
@@ -7,10 +7,25 @@ export const actions = {
       commit('SET_ERROR', e, { root: true });
     }
   },
+  async fetchAdminById({ commit }, id) {
+    try {
+      const { data } = await this.$axios.get('./mock/posts.json');
+      const post = data.find((post) => post._id === id);
+      return post;
+    } catch (e) {
+      commit('SET_ERROR', e, { root: true });
+    }
+  },
   async remove({}, id) {
   // Удаляем пост на сервере
     setTimeout(() => {
       console.log('Пост удален:', id);
+    }, 1000);
+  },
+  async update({}, { id, text }) {
+    // Обновляем пост на сервере
+    setTimeout(() => {
+      console.log('Пост обновлен:', id, text);
     }, 1000);
   },
 };
