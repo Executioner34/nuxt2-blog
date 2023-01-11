0bfd31c11d921a4d77f82de0e807a61915c3a7b2
Alexander Gorlenko <goral92@inbox.ru>
2023-01-11 14:47:08 +0300

    refactor: удалит неиспользуемую функцию RTI-2133

--- helpers/currentDate.js
+++
@@ -1,8 +0,0 @@
-/**
- * @module helpers/currentDate.js
- * @desc - возвращает текущую дату в виде строки
- */
-
-const currentDate = () => new Date().toLocaleString();
-
-export default currentDate;
